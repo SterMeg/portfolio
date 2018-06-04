e4a1e1af60bf7967a691bfa4f3128cde39eff994
Meghan Sterling <sterling.meghan@gmail.com>
2018-06-03 23:13:06 -0400

    more restyling of buttons etc

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,7 +1,8 @@
 
 $(function () {
+    //Create typing effect
     new TypeIt('.type-it', {
-        strings: ["Web Developer", "Comic Enthusiast"],
+        strings: ["Front-End Developer", "Enthusiastic Learner", "Avid Googler", "Pug Lover", "Comic Book Fanatic", "Coffee Consumer", "Board Game Geek", "MMO Master"],
         breakLines: false,
         cursor: false,
         loop: true
@@ -10,19 +11,12 @@ $(function () {
     //Apply smooth scroll
     $('a').smoothScroll({ offset: 100, speed: 500 });
 
+    //Add classes to change fixed nav on scroll
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
             $(".navigation-copy").addClass('scroll');
         } else {
             $(".navigation-copy").removeClass("scroll");
         }
-    });
-
-    // Animation to fill progress bar on mouseover
-    $('.progressBackground').on('mouseover', function () {
-        $(".progressBar", this).addClass('filled');
-    });
-    $('.progressBackground').on('mouseleave', function () {
-        $(".progressBar", this).removeClass('filled');
     });
 });
