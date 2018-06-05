3fbc0e0af0b9ef5ce128ea7fe2b543f2e4c8d66b
Meghan Sterling <sterling.meghan@gmail.com>
2018-06-05 10:13:13 -0400

    made a couple small stylefixes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,19 +2,20 @@
 $(function () {
     //Create typing effect
     new TypeIt('.type-it', {
-        strings: ["Front-End Developer", "Enthusiastic Learner", "Avid Googler", "Pug Lover", "Comic Book Fanatic", "Coffee Connoisseur", "Board Game Geek", "MMO Master"],
+        strings: ["Front-End Developer", "Enthusiastic Learner", "Avid Googler", "Pug Lover", "Comic Book Fanatic", "Coffee Connoisseur", "Board Game Geek", "Shower Singer"],
         breakLines: false,
         cursor: false,
         loop: true
     });
 
     //Apply smooth scroll
-    $('a').smoothScroll({ offset: 100, speed: 500 });
+    $('a').smoothScroll({ speed: 500 });
 
     $('.responsive-nav-icon').on('click touch', function() {
         console.log('clicked');
         $('.menu').toggleClass('show-menu');
         $('.responsive-nav-icon').toggleClass('change');
+        $('.bar').toggleClass('animate');
     });
 
     //Add classes to change fixed nav on scroll
